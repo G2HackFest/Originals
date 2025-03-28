2381c04765391b0a17c8bf95be3674674efc89fc
shyam <meghashyamreddy014@gmail.com>
2025-03-29 02:36:10 +0530

    fixed bugs in Landing page

--- ScholarSpend/src/App.jsx
+++ ScholarSpend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import LandingPage from './pages/Landing';
-// import Authenticate from './pages/Authenticate';
+import Authenticate from './pages/Authenticate';
 // import Userhome from './pages/Userhome';
 // import Profile from './pages/Profile';
 function App() {
@@ -9,8 +9,8 @@ function App() {
    <BrowserRouter>
    <Routes>
     <Route path="/" element={<LandingPage/>}/>
-    {/* <Route path="/authenticate" element={<Authenticate/>}/>
-    <Route path="/userhome" element={<Userhome/>}/>
+    <Route path="/authenticate" element={<Authenticate/>}/>
+    {/* <Route path="/userhome" element={<Userhome/>}/>
     <Route path="/profile" element={<Profile/>}/> */}
    </Routes>
    </BrowserRouter>
